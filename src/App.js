b16a5aed60ae4b307bccfc74dbd93ece6fd04c8b
Julien Pavon <julien.pavon@valtech.com>
2017-03-17 12:50:14 +0100

    Show tweet details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import React, { Component } from 'react';
 
+import Modal from 'react-modal';
 import io from 'socket.io-client';
 
 class App extends Component {
@@ -11,29 +12,44 @@ class App extends Component {
     let self = this;
     
     this.state = {
-      tweets: []
+      tweets: [],
+      tweetDetails: {
+          // id_str: "839900050331426818", 
+          // created_at: 'Fri Mar 10 20:20:43 +0000 2017',
+          // text: 'RT @arnaudhacquin: rvfrance: RT rBrillet: Foncez ! #HackValtech #Hackaton #VR #Reality cc Valtech_FR https://t.co/llBBxxbG8h', 
+          // entities: {
+          //   // media: [{ 
+          //   //   media_url: 'https://pbs.twimg.com/media/Ck6kfxWXIAA9P6t.jpg' 
+          //   // }],
+          // },
+          // user: {
+          //   name: "What's my mothaf*****g name ?",
+          //   screen_name: '_snoopDogg',
+          //   profile_image_url: "http://pbs.twimg.com/profile_images/840613476414435329/33DmyxUe_normal.jpg"
+          // }
+      },
+      modalIsOpen: false
     };
 
-    for (let i = 1; i <= 10; i++) {
-      this.state.tweets.push(
-        {
-          id_str: i, 
-          created_at: 'Fri Mar 10 20:20:43 +0000 2017',
-          text: 'RT @arnaudhacquin: rvfrance: RT rBrillet: Foncez ! #HackValtech #Hackaton #VR #Reality cc Valtech_FR https://t.co/llBBxxbG8h', 
-          entities: {
-            media: [{ 
-              media_url: 'https://pbs.twimg.com/media/Ck6kfxWXIAA9P6t.jpg' 
-            }],
-          },
-          user: {
-            name: "What's my mothaf*****g name ?",
-            screen_name: '_snoopDogg',
-            profile_image_url: "http://pbs.twimg.com/profile_images/840613476414435329/33DmyxUe_normal.jpg"
-          }
+    // for (let i = 1; i <= 10; i++) {
+    //   this.state.tweets.push(
+    //     {
+    //       id_str: i, 
+    //       created_at: 'Fri Mar 10 20:20:43 +0000 2017',
+    //       text: 'RT @arnaudhacquin: rvfrance: RT rBrillet: Foncez ! #HackValtech #Hackaton #VR #Reality cc Valtech_FR https://t.co/llBBxxbG8h', 
+    //       entities: {
+    //         // media: [{ 
+    //         //   media_url: 'https://pbs.twimg.com/media/Ck6kfxWXIAA9P6t.jpg' 
+    //         // }],
+    //       },
+    //       user: {
+    //         name: "What's my mothaf*****g name ?",
+    //         screen_name: '_snoopDogg',
+    //         profile_image_url: "http://pbs.twimg.com/profile_images/840613476414435329/33DmyxUe_normal.jpg"
+    //       }
 
-        });
-    }
-
+    //     });
+    // }
 
     this.socket = io();
     this.socket.on('allTweets', function(tweets) {
@@ -49,6 +65,7 @@ class App extends Component {
   }
 
   showTweet(id) {
+    //this.setState({modalIsOpen: true});
     console.log(`Show tweet with id '${id}'`);
 
     let self = this;
@@ -57,14 +74,18 @@ class App extends Component {
     this.socket.on('tweetDetails', function(tweet) {
       console.log('Get tweet details : ');
       console.log(tweet);
-      self.setState({tweetDetails: tweet});
+      self.setState({tweetDetails: tweet, modalIsOpen: true});
     });
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
   }
   
   render() {
     let tweets = this.state.tweets.map((tweet) => {
       let image = {
-        background: tweet.entities.media && tweet.entities.media.length > 0 ? `url(${tweet.entities.media[0].media_url}:small) center` : ''
+        background: tweet.entities.media && tweet.entities.media.length > 0 ? `url(${tweet.entities.media[0].media_url}:small) no-repeat center` : ''
       };
 
       return <li onClick={this.showTweet.bind(this, tweet.id_str)} key={tweet.id_str} className="tweet-item">
@@ -78,6 +99,17 @@ class App extends Component {
         </div>
       </li>;
     });
+
+    const customStyles = {
+      overlay: {
+        backgroundColor: 'rgba(2, 2, 2, 0.4)'
+      },
+      content : {
+        padding: '30px 10px',
+        overflow: 'hidden'
+      }
+    };
+
     return (
       <div className="App">
         <div className="App-header">
@@ -88,6 +120,22 @@ class App extends Component {
           <ul className="tweets-collection">
             {tweets}
           </ul>
+
+          <Modal isOpen={this.state.modalIsOpen}
+                 onRequestClose={this.closeModal}
+                 style={customStyles}
+                 contentLabel="">
+
+            <button className="close-modal-button" onClick={this.closeModal.bind(this)}>X</button>
+            <div className="tweet-details-box">
+              <div className="tweet-details-image" style={{background: this.state.tweetDetails.entities.media && this.state.tweetDetails.entities.media.length > 0 ? `url(${this.state.tweetDetails.entities.media[0].media_url}:medium) no-repeat center` : ''}}>
+              </div>
+              <div className="tweet-details-content">
+                <div className="tweet-details-content-user">{this.state.tweetDetails.user.name} @({this.state.tweetDetails.user.screen_name})</div>
+                <div className="tweet-details-content-text">{this.state.tweetDetails.text}</div>
+              </div>
+            </div>
+          </Modal>
         </div>
       </div>
     );
